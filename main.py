b5ba26c1b70f9152895cd55ba6f2138a97fd45b8
bolokfabio <bolok.fabio@iisgalvanimi.edu.it>
2026-04-13 07:45:13 +0000

    funziona orario

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from datetime import datetime
+
+app = FastAPI()
+
+# Serve per rendere accessibili CSS e JS
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def home():
+    return FileResponse('static/home.html')
+
+@app.get("/ora")
+def dammi_ora():
+    return {"orario": datetime.now().strftime("%H:%M:%S")}
+
+# NUOVO: Endpoint con parametro di query
+@app.get("/saluta")
+def saluta_utente(nome: str):
+    return {"messaggio": f"Ciao {nome}, benvenuto nel server di Terza!"}
